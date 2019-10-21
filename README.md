fb73114f284bfd29ea5ca730945da70c13087e4a
suhanyujie <suhanyujie@qq.com>
2019-10-21 09:29:33 +0800

    提交一些配置文件

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-simple-script
+# simple-script
+* 学习[《编译原理之美》](https://time.geekbang.org/column/intro/219)的最佳实践。
+
---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 2019-10-21
+ * Time: 09:25
+ */
+
+/**
+ * 编写读取源码相关逻辑
+ *
+ */
+
+
